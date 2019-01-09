bcc5052cc32f62afe349f285c9fccc7a5e01d775
jimmytu <jimmytutron@gmail.com>
2019-01-09 11:55:27 -0800

    re-add hide for work quotes, fix index

--- assets/js/magic.js
+++ assets/js/magic.js
@@ -1,9 +1,11 @@
 $("#hello").hide();
+$("#work").hide();
 $("#follow").hide();
 $(".footer").hide();
 
 $("#about").on("click", function() {
     $("#hello").show();
+    $("#work").hide();
     $("#follow").hide();
     $(".footer").show();
 
@@ -14,6 +16,7 @@ $("#about").on("click", function() {
 
 $("#port-gallery").on("click", function() {
     $("#hello").hide();
+    $("#work").show();
     $("#follow").hide();
     $(".footer").hide();
 
@@ -24,10 +27,23 @@ $("#port-gallery").on("click", function() {
 
 $("#gallery-design").on("click", function() {
     $("#hello").hide();
+    $("#work").show();
     $("#follow").hide();
     $(".footer").hide();
 
     $.get("gallery-design.html", function(data) {
         $("#content").html(data);
     });
+});
+
+$("#contact").on("click", function() {
+    $("#hello").hide();
+    $("#work").hide();
+    $("#follow").show();
+    $(".footer").show();
+
+    $.get("contact.html", function(data) {
+        $("#content").html(data);
+    });
+
 });
